dea2e3b2c172c36d666cd2f93e2829e86d0a4fa6
nicopIII <nicolaspe@lyceechurchill.london>
2024-09-24 14:24:13 +0000

    commit2

--- Debuter avec Python/exercice_4.py
+++ Debuter avec Python/exercice_4.py
@@ -1,11 +1,26 @@
-# Exercice 4 - question 6
+import random
 
-cote_du_carre = 3
-c = cote_du_carre
-p = 4*c
-a = c^2
-b = (a > 5)
-print(c)
-print(p)
-print(a)
-print(b)
+
+#Trouver l'aire et le perimetre d'un carre
+c = random.randint(1, 10)
+P = c * 4
+A = c ** 2 
+b = A > 5 
+print("On prend ", c, "comme la longueure d'un cote")
+print("Le perimetre du carre est de", P)
+print("L'aire du carre est de", A )
+print("L'aire du carre est superieur a 5 ?", b)
+
+# Fonction pour calculer le perimetre d'un carre
+x = random.randint(1, 10)
+def perimetre(x):
+    return x * 4 
+
+
+print("Le perimetre du carre, calculer avec une fonction, en utilisant", x, "comme longueure d'un cote, est de", perimetre(x))
+
+#Fonction pour calculer l'air d'un carre
+def surface(x):
+    return x ** 2 
+
+print("L'aire dur carre, calculer avec une fonction, en utilisant", x, "comme longueure d'un cote, est de", surface(x))
