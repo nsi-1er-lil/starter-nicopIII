5d1090ffc0fc6d778328f4aa262c12ed6c0067b0
nicopIII <nicolaspe@lyceechurchill.london>
2024-09-26 19:34:38 +0100

    Complet exo5devoir.py

--- Debuter avec Python/exo5devoir.py
+++ Debuter avec Python/exo5devoir.py
@@ -1,8 +1,18 @@
 # Exercice 5
 
 import random
+import math
+#Quelle est la formule permettant de calculer le périmètre d'un cercle ?
+print("1. La formule pour calculer le perimetre d'une cercle est: 2πr ")
 
+#Quel argument doit prendre en entrée la fonction périmètre_cercle ? Quel serait le type de cet argument ?
+print("2. L'argument est le rayon du cercle. Cet argument serait de type float")
+
+#Compléter l'algorithme de la fonction ci-dessus afin qu'elle renvoie le périmètre d'un cercle.
 r = random.randint(1, 10)
-pi = 3.14159  # Approximation de pi
-P = pi * r * 2  # Calcul du périmètre
-print("Le périmètre du cercle, calculé avec une fonction, sachant que le rayon est de", r, "fait", P)
+pi = (math.pi)
+def perimetre_cercle(r):
+    perimetre = 2 * pi * r
+    perimetreApprox = round(perimetre, 2)
+    return perimetreApprox
+print("Le perimetre du cercle de rayon", r , "est de environ ", perimetre_cercle(r))
