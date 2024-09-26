d57971b35b33af95b6a7bab1ba5ea4756ebcd288
nicopIII <nicolaspe@lyceechurchill.london>
2024-09-26 16:20:40 +0100

    Delete Debuter avec Python/devoirs27sep.py

--- Debuter avec Python/devoirs27sep.py
+++
@@ -1,9 +0,0 @@
-import random
-
-# Exercice 5 
-r = random.randint(1,10)
-pi = 3.14159 # Approximation de pi
-perimetre = P
-P = pi * r * 2
-
-print ("Le perimetre du cercle, calculer avec une fontion, sachant que le rayon est de", r, "fait", P.)
