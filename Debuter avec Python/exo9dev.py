227f7ae92d2d3ec37335b7e5a96b4aa8a4eb8830
nicopIII <nicolaspe@lyceechurchill.london>
2024-09-26 19:40:46 +0100

    Complet exo9dev.py

--- Debuter avec Python/exo9dev.py
+++ Debuter avec Python/exo9dev.py
@@ -1,1 +1,14 @@
+# Exercice 9
 
+import random
+def aMoyenne(vI, vF, t):
+
+    # Calcul de l'accélération moyenne
+    a = (vF - vI) / t
+    aApprox = round(a, 2)
+    return aApprox
+vI = random.randint(1, 50)
+vF = random.randint(1, 50)
+t = random.randint(5, 30)
+
+print("L'acceleration moyenne avec une vitesse initiale de", vI, "m/s et une vitesse finale de", vF, "m/s sur une duree de", t, "secondes est de", aMoyenne(vI, vF, t), "m/s²")
