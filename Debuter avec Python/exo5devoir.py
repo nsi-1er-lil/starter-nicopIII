d107589ee4b665878b8419771d1c48d4776327c1
nicopIII <nicolaspe@lyceechurchill.london>
2024-09-26 16:27:38 +0100

    Update exo5devoir.py

--- Debuter avec Python/exo5devoir.py
+++ Debuter avec Python/exo5devoir.py
@@ -1,9 +1,7 @@
 import random
 
-# Exercice 5 
-r = random.randint(1,10)
-pi = 3.14159 # Approximation de pi
-perimetre = P
-P = pi * r * 2
-
-print ("Le perimetre du cercle, calculer avec une fontion, sachant que le rayon est de", r, "fait", P.)
+# Exercice 5
+r = random.randint(1, 10)
+pi = 3.14159  # Approximation de pi
+P = pi * r * 2  # Calcul du périmètre
+print("Le périmètre du cercle, calculé avec une fonction, sachant que le rayon est de", r, "fait", P)
