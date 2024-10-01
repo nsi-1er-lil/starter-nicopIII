445d8c569fbcdbd34ff5a1ed6b3c4564558a5f57
nicopIII <nicolaspe@lyceechurchill.london>
2024-10-01 13:17:09 +0000

    commit final exo3

---
+++ Debuter avec Python/exercice_dev2exo3.py
@@ -0,0 +1,12 @@
+#exercice 3
+def pokemon_choisi(pokemon):
+    if  pokemon == a :
+        print('Pokemon choisi : Bulbizarre. Pokemon rival : Carapuce')
+    elif pokemon == b:
+        print('Pokemon choisi : Carapuce. Pokemon rival : Salameche')
+    else :
+        print('Pokemon choisi : Salameche. Pokemon rival : Bulbizarre')
+a = 'bulbizar'
+b = 'carapuce'
+c = 'salameche'
+pokemon_choisi(a),pokemon_choisi(b), pokemon_choisi(c)
