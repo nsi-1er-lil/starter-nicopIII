a2740c2490a4b40522d4bb9599941ad302d05bb6
nicopIII <nicolaspe@lyceechurchill.london>
2024-10-01 13:18:51 +0000

    commit final exo 2

---
+++ Debuter avec Python/exercice_dev2exo1.py
@@ -0,0 +1,8 @@
+# exo 1:
+
+import random
+taille = random.randit(0,270)
+if taille > 150 :
+    print(taille, 'cm : Vous pouver entrer, ne vomissez pas svp.')
+else:
+    print(taille, 'cm : Vous etes trop petits, allez manger de la soupe.')
